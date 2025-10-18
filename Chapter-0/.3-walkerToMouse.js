90fec02c71231bf09acc509ba1a3a0ad755e90f1
nickc124 <nickc14@nycstudents.net>
2025-10-18 10:23:50 -0400

    Create .3-walkerToMouse.js

---
+++ Chapter-0/.3-walkerToMouse.js
@@ -0,0 +1,11 @@
+/*Create a random walker with dynamic probabilities.
+For example, can you give it a 50 percent chance of moving in the direction of the mouse?
+Remember, you can use mouseX and mouseY to get the current mouse position in p5.js!
+*/
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
